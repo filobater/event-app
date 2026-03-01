71e61a5f3621836acff5b2304b08c82b046584e6
filobater <filobaternader14@gmail.com>
2026-03-01 23:23:25 +0200

    remove unwanted inputs from search bar

--- apps/client/src/app/shared/components/inputs/search-input/search-input.component.ts
+++ apps/client/src/app/shared/components/inputs/search-input/search-input.component.ts
@@ -6,14 +6,5 @@ import { Component, input } from '@angular/core';
   templateUrl: './search-input.component.html',
 })
 export default class SearchInputComponent {
-  private static idCounter = 0;
-  readonly inputId = `search-input-${++SearchInputComponent.idCounter}`;
-
-  id = input<string>('');
-  labelText = input<string>('');
   placeholder = input<string>('Search');
-
-  protected get resolvedId() {
-    return this.id() || this.inputId;
-  }
 }
